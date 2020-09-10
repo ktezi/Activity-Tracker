08aa617b1e6cfede4345d7be9254ff770fb6ae90
Tezi <tezikhan1@gmail.com>
2020-09-10 23:45:54 +0530

    Updates

---
+++ precache-manifest.86891777333284891b4148dea199dacb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e5713f2c1ceabcef8b99acff134381",
+    "url": "/Activity-Tracker/index.html"
+  },
+  {
+    "revision": "34337cec1624a4f9e142",
+    "url": "/Activity-Tracker/static/css/2.055e9163.chunk.css"
+  },
+  {
+    "revision": "54ed734244687147c1ec",
+    "url": "/Activity-Tracker/static/css/main.c593b6d4.chunk.css"
+  },
+  {
+    "revision": "34337cec1624a4f9e142",
+    "url": "/Activity-Tracker/static/js/2.e4ef6707.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Activity-Tracker/static/js/2.e4ef6707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ed734244687147c1ec",
+    "url": "/Activity-Tracker/static/js/main.3f2b5ce0.chunk.js"
+  },
+  {
+    "revision": "6342d40ba98d2c46d1f7",
+    "url": "/Activity-Tracker/static/js/runtime-main.f5739777.js"
+  }
+]);
